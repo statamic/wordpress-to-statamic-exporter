85fef06816680c2c905ae3bc64eb10640b9deeea
Jaggy Gauran <i.am@jag.gy>
2018-02-10 04:36:46 +0800

    Hello Wordpress, my old friend.

---
+++ form.php
@@ -0,0 +1,36 @@
+<div class="wrap">
+    <h1>Statamic JSON Exporter</h1>
+
+    <p>When you click the button below the plugin will export a JSON file that you can import to your Statamic application.</p>
+
+    <p>Once you’ve saved the download file, you can enjoy the flat file goodness.</p>
+
+    <h2>Choose what to export</h2>
+
+    <form method="POST" action="<?php echo get_site_url(); ?>/wp-content/plugins/wordpress-exporter/export.php">
+
+        <fieldset>
+            <input type="hidden" name="pathname" value="<?php echo get_home_path(); ?>">
+
+            <p>
+                <label><input type="checkbox" name="post" value="post">Post</label>
+            </p>
+
+            <p>
+                <label><input type="checkbox" name="page" value="page">Page</label>
+            </p>
+
+            <p>
+                <label><input type="checkbox" name="settings" value="settings">Settings</label>
+            </p>
+
+            <p>
+                <label><input type="checkbox" name="taxonomies" value="taxonomies">Taxonomies</label>
+            </p>
+        </fieldset>
+
+
+        <?php submit_button( 'Download JSON File' ); ?>
+
+    </form>
+</div>
---
+++ index.php
@@ -0,0 +1,7 @@
+<script type="text/javascript" src="<?php echo get_site_url() ?>/wp-content/plugins/jsonExport/js/jquery-1.11.2.min.js"></script>
+<script type="text/javascript">
+  jQuery(document).ready(function(){  
+  	jQuery("#menu-posts-export a:not(:last)").attr('href',"#"); 
+    jQuery("#menu-posts-export ul li:not(:last)").hide();    
+  })
+</script>
